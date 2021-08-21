c5964ac1c62415ef53d83c4d39b2f497bf5d72c8
ssong10 <leesy1403@naver.com>
2021-08-21 19:47:26 +0900

    Fix: Login Title

--- src/components/todo/TodoLogin.tsx
+++ src/components/todo/TodoLogin.tsx
@@ -4,26 +4,36 @@ import Spinner from "components/common/Spinner";
 import { Button,Input } from "antd";
 import styled from 'styled-components'
 import { errorInfo } from "components/common/Modal";
+const Header = styled.div`
+  font-size: 36px;
+  margin: 50% 0;
+  color: #119955;
+`
 const Center = styled.div`
   display:flex;
   height:100%;
   justify-content:center;
-  align-items:center;
 `
 const PasswordInput = styled(Input)`
   width:100%;
   margin-bottom: 12px;
 `
 const LoginContainer = styled.form`
+  height: 100%;
   text-align: center;
 `
-const fetchLogin = ():Promise<boolean> => {
-  return new Promise(resolve => {
+const fetchLogin = (password: string):Promise<boolean> => {
+  return new Promise((resolve) => {
     setTimeout(()=> {
-      resolve(true)
+      if (password === PASSWORD) {
+        resolve(true)
+      } else {
+        resolve(false)
+      }
     },500)
   })
 }
+
 interface LoginProps {
   setIsLogged: (state:boolean)=> void
 }
@@ -32,16 +42,16 @@ const PASSWORD = 'a1'
 const TodoLogin:React.FC<LoginProps> = ({ setIsLogged }) => {
   const [ isLoading , setIsLoading ] = useState(false)
   const [ password, setPassword ] = useState('')
-
-  const validation = () => password === PASSWORD
   const login = async () => {
-    if (validation()){
-      setIsLoading(true)
-      const isLogin = await fetchLogin()
+    setIsLoading(true)
+    const isLogin = await fetchLogin(password)
+    if (isLogin) {
       setIsLoading(false)
       setIsLogged(isLogin)
     } else {
       errorInfo(`비밀번호가 틀렸습니다\n비밀번호는 ${PASSWORD}입니다`)
+      setIsLoading(false)
+      setPassword('')
     }
   }
   const handlerChange = (e:React.ChangeEvent<HTMLInputElement>) => {
@@ -54,6 +64,9 @@ const TodoLogin:React.FC<LoginProps> = ({ setIsLogged }) => {
   }
   const LoginLayout = (
     <LoginContainer onSubmit={handlerSubmit}>
+      <Header>
+        <span>TodoList</span>
+      </Header>
       <PasswordInput placeholder="암호를 입력해주세요" type="password" value={password} onChange={handlerChange}/>
       <Button onClick={handlerSubmit}>로그인</Button>
     </LoginContainer>
